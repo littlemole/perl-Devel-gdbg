99d58ffffdf88d21f3050c04e278f8039b9b27c4
mike <mike@oha7.org>
2026-02-05 21:45:35 +0100

    truncate variables for tree display

--- docker/gdbgui/run.sh
+++ docker/gdbgui/run.sh
@@ -6,8 +6,8 @@ Xvfb $DISPLAY -screen 0 "${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH}" &
 sleep 1
 x11vnc -display $DISPLAY -forever -nopw -quiet -listen 0.0.0.0 -rfbport "$VNC_PORT" -xkb &
 sleep 1
-+mkdir -p /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml
-+cp /opt/xsettings.xml /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+mkdir -p /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml
+cp /opt/xsettings.xml /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 startxfce4 &
 sleep 1
 /usr/share/novnc/utils/novnc_proxy --vnc "127.0.0.1:$VNC_PORT" --listen "0.0.0.0:$NOVNC_PORT" &
